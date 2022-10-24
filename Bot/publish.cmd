9d5118aff1bc73ce69da088113116cf38d38b1bd
albertomollica <51949153+albertomollica@users.noreply.github.com>
2022-10-24 16:11:35 +0200

    second commit

--- Bot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABotAllFeatures.sln -p:DeployOnBuild=true -p:PublishProfile=.\3fsazurebot-Web-Deploy.pubxml -p:Password=hvNY1Tz0QNAnbdn1TNi8oLtiCXYfBFSKN8RlL29cHrWEXFADihoKh8DPTAEs
-
