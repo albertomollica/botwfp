5319e07bb1743e67f49592c2f1b77bb21eb3a5bc
AlbertoMollica <amollica@progesoftware.it>
2022-10-24 15:37:43 +0200

    first commit

---
+++ Bot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotAllFeatures.sln -p:DeployOnBuild=true -p:PublishProfile=.\3fsazurebot-Web-Deploy.pubxml -p:Password=hvNY1Tz0QNAnbdn1TNi8oLtiCXYfBFSKN8RlL29cHrWEXFADihoKh8DPTAEs
+
